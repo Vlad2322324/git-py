ad5f0a913a9cdc7184744eb1331cfb336573210a
Vlad2322324 <romantsov_vlad@mail.ru>
2023-04-13 14:26:11 +0700

    Сделал

---
+++ 11.py
@@ -0,0 +1,20 @@
+import random
+import itertools
+
+
+def randf(n):
+    return [random.uniform(0, n) for h in range(n)]
+
+
+def chained(a, b):
+    return list(itertools.chain(a, b))
+
+
+def responses_creator(item_ids):
+
+    return [dict(item_id=item) for item in item_ids]
+
+
+print(randf(2))
+print(chained([1, 4], [4, 23]))
+print(responses_creator([12, 21, 13]))
